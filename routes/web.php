c65b5d0f831951891d0a2f120b44b2a92c23913c
LucasPizzaia <lucas.pizzaia@edu.unifil.br>
2025-09-19 23:35:15 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Rota inicial -> redireciona para Professores se logado
 Route::get('/', function () {
     if (auth()->check()) {
         return redirect()->route('professores.index');
@@ -22,27 +21,23 @@ Route::get('/', function () {
     ]);
 });
 
-// Rotas protegidas (usuário precisa estar logado e verificado)
 Route::middleware(['auth', 'verified'])->group(function () {
-    // Professores
     Route::resource('professores', ProfessorController::class)
         ->parameters(['professores' => 'professor']);
 
-    // Turmas
+    
     Route::resource('turmas', TurmaController::class)
         ->parameters(['turmas' => 'turma']);
 
-    // Unidades Curriculares
     Route::resource('unidades-curriculares', UnidadeCurricularController::class)
         ->parameters(['unidades-curriculares' => 'unidadeCurricular']);
 
 
 
-    // Perfil
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Auth padrão (login, registro, etc.)
 require __DIR__.'/auth.php';
